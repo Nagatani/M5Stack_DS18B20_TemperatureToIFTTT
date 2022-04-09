da9e7098689920ec42619b2871d401aaed10b34b
Nagatani <Nagatani@users.noreply.github.com>
2022-04-09 14:31:31 +0900

    コードを見直してSerialの削除とかWifiMultiへの切り替えなど

--- SlackWebhooks.cpp
+++ SlackWebhooks.cpp
@@ -1,8 +1,6 @@
 #include "SlackWebhooks.h"
 
 #include <Arduino.h>
-#include <WiFi.h>
-#include <WiFiClient.h>
 #include <HTTPClient.h>
 #include "config.h"
 #include "WiFiMulti.h"
@@ -11,28 +9,27 @@ SlackWebhooks::SlackWebhooks() {
 }
 
 void SlackWebhooks::sendToSlack(String text) {
-  WiFiClient client;
   HTTPClient http;
 
   WiFiMulti wifiMulti;
   wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
 
-  Serial.print("conecting to wifi.");
+  // Serial.print("conecting to wifi.");
   while (wifiMulti.run() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    // Serial.print(".");
   }
-  Serial.println("wifi connected!");
+  // Serial.println("wifi connected!");
 
   // POST TO Slack Incoming Webhooks
   String json = "{\"text\":\"" + text + "\"}";
-  Serial.println(json);
+  // Serial.println(json);
 
   http.begin(WEBHOOKS_HOST, 443, WEBHOOKS_TRIGGER_PATH, SLACK_ROOT_CA);
   http.addHeader("Content-Type", "application/json" );
   http.POST((uint8_t*)json.c_str(), strlen(json.c_str()));
 
   // 送信成功かどうかのチェックはしない
-  Serial.println("Send Complete. : Slack Incoming WebHooks");
+  // Serial.println("Send Complete. : Slack Incoming WebHooks");
 
 }
